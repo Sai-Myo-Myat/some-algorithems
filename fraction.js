ecc5f0a05189c95448789d3018a961325a05e177
Sai Myo Myat <saimyomyat511@gmail.com>
2022-08-06 21:23:56 +0630

    time conversion & minMaxSum

--- fraction.js
+++ fraction.js
@@ -1,5 +1,5 @@
 //fraction of positve, negative and zero in an array
-const arr = [1,-1,0,2,-3];
+// const arr = [1,-1,0,2,-3];
 
 const fraction = (arr) => {
   let positive = 0;
@@ -23,3 +23,51 @@ const fraction = (arr) => {
   console.log(neg.toFixed(6));
   console.log(zer.toFixed(6));
 }
+
+//minSum and maxSum of 4 of 5 integers
+function miniMaxSum(arr) {
+  // Write your code here
+  //set minNum and maxNum 
+  //loop and sum all numbers
+  let minNum = arr[0];
+  let maxNum = arr[0];
+  let sum = 0;
+  for(let i=0; i< arr.length; i++){
+      if(minNum > arr[i]) {
+          minNum = arr[i]
+      }else if(maxNum < arr[i]){
+          maxNum = arr[i]
+      }
+      sum += arr[i];
+  }
+  const minSum = sum - maxNum;
+  const maxSum = sum - minNum;
+  console.log(minSum + " " + maxSum)
+}
+
+//time conversion
+function timeConversion(s) {
+  // Write your code here
+  //get AM or PM from s
+  //AM --> 12AM  => 00 / 
+  //PM --> 12 > _ => +12 / 12 => 12
+  const amOrPm = s.chatAt(8);
+  let hour = s.slice(0,2);
+  let militaryHour = "";
+
+  if(amOrPm === "A") {
+    if(hour === "12") {
+      militaryHour = "00";
+    }else {
+      militaryHour = hour;
+    }
+  }else {
+    if(hour === "12"){
+      militaryHour = hour;
+    }else {
+      militaryHour = parseInt(hour) + 12
+    }
+  }
+  return militaryHour.toString() + s.slice(2,8)
+}
+
